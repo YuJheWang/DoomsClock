fe9ffa3db3f0e01358cc3137a6941f1ee88d11d4
YuJheWang <yuzhewang0722@gmail.com>
2024-02-03 23:45:10 +0800

    add src

--- src/main.cpp
+++ src/main.cpp
@@ -1,46 +1,55 @@
+#include <glad/glad.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_opengl.h>
 
 #include <iostream>
 
 SDL_Window* window = nullptr;
-SDL_Renderer* rootRender = nullptr;
-SDL_Surface* root = nullptr;
+
+SDL_GLContext glContext = nullptr; 
 
 int main(int argc, char* argv[])
 {
     SDL_Init(SDL_INIT_EVERYTHING);
 
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MAJOR_VERSION, 4);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_MINOR_VERSION, 3);
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+
     window = SDL_CreateWindow(
         "Dooms Clock", 
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         1920, 1080,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN
-    ); 
-    if (window == nullptr) 
+        SDL_WINDOW_SHOWN | SDL_WINDOW_FULLSCREEN |SDL_WINDOW_OPENGL
+    );
+    if (window == nullptr)
     {
         std::cerr << "Fail to create window!" << std::endl;
         SDL_Quit();
         return -1;
     }
 
-    root = SDL_GetWindowSurface(window);
-    if (root == nullptr) 
+    glContext = SDL_GL_CreateContext(window);
+    if (glContext == nullptr)
     {
-        std::cerr << "Fail to create root surface!" << std::endl;
+        std::cerr << "Fail to create OpenGL context!" << std::endl;
         SDL_Quit();
         return -1;
     }
 
-    rootRender = SDL_CreateSoftwareRenderer(root);
-    if (rootRender == nullptr)
+    if (!gladLoadGLLoader((GLADloadproc) SDL_GL_GetProcAddress))
     {
-        std::cerr << "Fail to create root renderer!" << std::endl;
+        std::cerr << "Fail to load GLAD!" << std::endl;
         SDL_Quit();
         return -1;
     }
+
+    int width, height;
+    SDL_GetWindowSize(window, &width, &height);
+    glViewport(0, 0, width, height);
 
     bool running = true;
-    while (running) 
+    while (running)
     {
         SDL_Event event;
         while (SDL_PollEvent(&event))
